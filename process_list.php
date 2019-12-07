abe24750fcb0f01cdf3a7d9b437a160b7b8cbb6e
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2019-12-07 11:12:38 +0200

    update

---
+++ process_list.php
@@ -0,0 +1,25 @@
+<?php
+    function process_list(&$array){
+        $file="trains.txt";
+
+        $fopen = fopen($file, r);
+
+        $fread = fread($fopen,filesize($file));
+
+        fclose($fopen);
+
+        $remove = "\n";
+
+        $split = explode($remove, $fread);
+
+        $array[] = null;
+        $tab = "\t";
+
+        foreach ($split as $string)
+        {
+            array_push($array,$string);
+        }
+
+    }
+
+?>
